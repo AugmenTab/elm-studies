b6fc1ede75f9aa59b71cb00ad7d477de5934a7ca
AugmenTab <tbaum92@live.com>
2021-01-10 19:35:58 -0700

    Moved JS code to dedicated files.

---
+++ photogroove/js/pasta.js
@@ -0,0 +1,14 @@
+var app = Elm.Main.init({
+    node: document.getElementById("app"),
+    flags: Pasta.version
+});
+
+app.ports.setFilters.subscribe(function(options) {
+    requestAnimationFrame(function() {
+        Pasta.apply(document.getElementById("main-canvas"), options);
+    });
+});
+
+Pasta.addActivityListener(function(activity) {
+    app.ports.activityChanges.send(activity);
+});
